30150556681628028dce984b3f3184e6f5123d4a
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2025-06-30 22:13:16 +0200

    Initial commit

---
+++ ChessClock_DeptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClock_DeptApp.swift
+//  ChessClock-Dept
+//
+//  Created by Ivan Jovanovik on 30.6.25.
+//
+
+import SwiftUI
+
+@main
+struct ChessClock_DeptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
